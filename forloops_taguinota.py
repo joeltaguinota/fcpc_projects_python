9509992e1665ccaf7a6a623f86865bc39b67aa12
joeltaguinota <45374737+joeltaguinota@users.noreply.github.com>
2018-11-27 07:59:31 +0800

    for loops

---
+++ forloops_taguinota.py
@@ -0,0 +1,22 @@
+#for loop by 2
+print("By 2")
+for x in range(2,12,2):
+    print(x)
+#for loop by 3
+print("By 3")
+for x in range(3,13,3):
+    print(x)
+#for loop by 4
+print("By 4")
+for x in range(4,17,4):
+    print(x)
+#for loop by 12
+print("By 12")
+for x in range(12,37,12):
+    print(x)
+
+print("Multiplication Table 10 - 20")
+for y in range(10,21):
+    for z in range(1,11):
+        print(z * y, end = '\t')
+    print()
