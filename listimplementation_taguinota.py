25a7e4fdf3a761f6084687155c9b361f17453ea9
joeltaguinota <45374737+joeltaguinota@users.noreply.github.com>
2018-11-27 10:08:53 +0800

    list implementation

---
+++ listimplementation_taguinota.py
@@ -0,0 +1,19 @@
+list_user = ["Curry","Rose","Irving","Westbrook","Durant"]
+jerser_number =["30","1","2","0","35"]
+bball_team = ["GSW","CHI","CLE","OKC","LAL"]
+
+for x in range(len(list_user)):
+    print(list_user[x],end= "\t")
+
+print()
+for y in range(len(jerser_number)):
+    print(jerser_number[y],end= "\t")
+print()
+for z in range(len(bball_team)):
+    print(bball_team[z],end= "\t")
+print()
+print(len(list_user),end= "\t")
+print()
+print(len(jerser_number),end= "\t")
+print()
+print(len(bball_team),end= "\t")
