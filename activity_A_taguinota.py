ceb06f2d3a89b8cdd4626de375022cfab6002cc4
joeltaguinota <45374737+joeltaguinota@users.noreply.github.com>
2018-11-27 11:07:12 +0800

    activity A

---
+++ activity_A_taguinota.py
@@ -0,0 +1,16 @@
+print("Activity 1")
+for x in range(1,17,3):
+    print(x ,end= "\t")
+print()
+
+print("Activity 2")
+for y in range(100,78,-7):
+    print(y,end= "\t")
+
+print()
+print("Activity 3")
+for z in range(1,5):
+    if z % 3 == 0:
+        print(1,end= "\t")
+    else:
+        print(0,end= "\t")
