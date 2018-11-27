4a46f8682268e843d356c10a9225343a585536b6
joeltaguinota <45374737+joeltaguinota@users.noreply.github.com>
2018-11-27 08:01:02 +0800

    dynamic input

---
+++ dynamicinput_taguinota.py
@@ -0,0 +1,14 @@
+#accepts input from name
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("You're name is: " + name)
+print("You're age is: " + age)
+
+#accepts numerical input -- int()
+fnum = int(input("Enter first number: "))
+snum = int(input("Enter second number: "))
+
+to_int_value = int("10")
+
+print(fnum + snum)
+print(to_int_value ** 2)
