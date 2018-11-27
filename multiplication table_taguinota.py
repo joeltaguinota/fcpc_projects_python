4c5cbc8525885a9e184902041a49346b80e70890
joeltaguinota <45374737+joeltaguinota@users.noreply.github.com>
2018-11-27 09:33:02 +0800

    multiplication table

---
+++ multiplication table_taguinota.py
@@ -0,0 +1,17 @@
+#for x in range(1,11):
+    #for y in range(1,11):
+        #z = x*y
+        #if z % 2 == 1:
+            #print("O",end= "\t")
+       # else:
+            #print(x*y,end= "\t")
+    #print()
+
+for x in range(1,11):
+    for y in range(1,11):
+        z = x*y
+        if z % 2 == 0:
+            print("Z",end= "\t")
+        else:
+            print(x*y,end= "\t")
+    print()
