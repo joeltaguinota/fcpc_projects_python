ec5528706332577a9adc85a0daef48bd772f8814
joeltaguinota <45374737+joeltaguinota@users.noreply.github.com>
2018-12-18 09:17:25 +0800

    recursion

---
+++ recursion_taguinota.py
@@ -0,0 +1,17 @@
+def populatelist():
+    listX = ["abc","def","ghi"]
+    return listX
+    
+listY = populatelist()
+print(listY)
+
+def reducelist():
+    listZ = populatelist()
+    listZ.remove("abc")
+    return listZ
+print(reducelist())
+
+def abc():
+    print("Hi")
+    abc()
+print(abc())
