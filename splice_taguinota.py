4cb5eeb2067130344d3a536192556ef2fee3a8d0
joeltaguinota <45374737+joeltaguinota@users.noreply.github.com>
2019-01-22 11:00:37 +0800

    Splice

---
+++ splice_taguinota.py
@@ -0,0 +1,8 @@
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numList[3:9])
+
+numlist2 = [0.1, 2.3, 3.5, 4.7, 2.4, 3.7, 5.3, 7.1, 2.9, 1.7,]
+print(numlist2[4:8])
+
+carsList = ["Toyota", "Mitsubushi", "Ford", "Nissan", "Subaru", "Kia", "Suzuki", "Isuzu", "Hyundai", "Honda"]
+print(carsList[2:6])
