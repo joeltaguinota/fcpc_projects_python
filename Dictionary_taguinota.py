9dc8a54fdc71117115a3d7fe107ac229746c53c4
joeltaguinota <45374737+joeltaguinota@users.noreply.github.com>
2019-02-12 09:20:55 +0800

    Dictionary

---
+++ Dictionary_taguinota.py
@@ -0,0 +1,10 @@
+myContactList = {}
+
+while True:
+    user_Input = input("Enter your name: ")
+    number_Input = input("Enter your number: ")
+    myContactList["Name"] = user_Input
+    myContactList["Number"] = number_Input
+    print(myContactList)
+
+
